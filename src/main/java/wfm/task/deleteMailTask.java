70709a688addfcc0543617315819ff5a4cc518e0
Oleg Denisov <ferus@chello.at>
2013-06-19 17:42:44 +0200

    multiple recipients tested The mail contains now also the Username and affected course name

--- src/main/java/wfm/task/deleteMailTask.java
+++ src/main/java/wfm/task/deleteMailTask.java
@@ -1,8 +1,9 @@
 package wfm.task;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
-import java.util.Set;
+
 
 import javax.inject.Named;
 import javax.mail.Message;
@@ -12,6 +13,8 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
@@ -20,28 +23,42 @@ import org.activiti.engine.delegate.JavaDelegate;
 @Named("deleteMail")
 public class deleteMailTask implements JavaDelegate {
 
-	private Set<String> userMails = new HashSet<String>();
-	
-	//private StringBuilder message = null;
-	
+	private List<String> userMails = new ArrayList<String>();
+	private List<String> courseNames = new ArrayList<String>();
+	private List<String> userNames = new ArrayList<String>();
+	  
 	private String add;
+	private String uName;
+	private String cName;
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 				
 		System.out.println("Found emails: "+execution.getVariable("deleteProcess"));
-		userMails = (HashSet<String>) execution.getVariable("deleteProcess");
-		
-		for(String mail: userMails){
-			add = mail;
+		System.out.println("Found courseNames: "+execution.getVariable("courseName"));
+		System.out.println("Found userNames: "+execution.getVariable("userName"));
+		userMails = (List<String>) execution.getVariable("deleteProcess");
+		courseNames = (List<String>) execution.getVariable("courseName");
+		userNames = (List<String>) execution.getVariable("userName");
+				
+		if(!userMails.isEmpty()){
 			
-			System.out.println("Sending mail...to: "+add);
-			initializeMailService(add);//.sendMessage();
-			System.out.println("done...");
-		}
-		
+			for(int i=0;i<userMails.size();i++){
+				add = userMails.get(i);
+				uName = userNames.get(i);		
+				cName = courseNames.get(i);
+				
+				System.out.println("Sending mail...to: "+add);
+				initializeMailService(add,uName,cName);
+				System.out.println("done...");
+			}		
+		}		
 	}
-	private void initializeMailService(String add) throws AddressException, MessagingException {
+	private void initializeMailService(String add, String uName, String cName) throws AddressException, MessagingException {
 		
 	   // Sender's email ID needs to be mentioned
 	   String from = "sscms.sender@gmail.com";
@@ -79,12 +96,12 @@ public class deleteMailTask implements JavaDelegate {
 	      message.setSubject("SCCMS: Course canceled!");
 
 	      // Now set the actual message
-	      message.setText("Dear user of the SCCMS!\n" +
-	      		"This Email is a notification that a course you have been subscribed to has been canceled!\n" +
+	      message.setText("Dear "+uName+"!\n" +
+	      		"This Email is a notification that a course you have been subscribed to has been canceled!\n\n" +
+	      		"The affected course is: "+cName+"\n"+
 	      		"Please feel free to select an alternative.\n\n" +
-	      		"this is an automatically generated course cancelling notification.\n\n" +
-	      		"Best regards,\nSCCMS");
-	    
+	      		"This is an automatically generated course cancelling notification.\n\n" +
+	      		"Best regards,\nSCCMS");	    
 
 	      // Send message
 	      Transport transport = session.getTransport("smtp");
