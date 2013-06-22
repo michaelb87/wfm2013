eb63ea8038fcde32ad2e1bc23a8e53d847d04a5e
Michael <mb@michaelb.at>
2013-06-22 21:31:49 +0200

    forgot delegate class

--- src/main/java/wfm/task/NotifyTrainerAboutWeather.java
+++ src/main/java/wfm/task/NotifyTrainerAboutWeather.java
@@ -4,12 +4,12 @@ import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 
 
-public class NotifyUserAboutCancellation implements JavaDelegate {
+public class NotifyTrainerAboutWeather implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 				
-		System.out.println("you should notify the user about the course cancellation now!");
+		System.out.println("you should notify the _trainer_ about the bad weather!");
 		
 	}
 	
---
+++ src/main/java/wfm/task/NotifyUserAboutCancelation.java
@@ -0,0 +1,16 @@
+package wfm.task;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+
+public class NotifyUserAboutCancelation implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+				
+		System.out.println("you should notify the user about the course cancellation now!");
+		
+	}
+	
+}
